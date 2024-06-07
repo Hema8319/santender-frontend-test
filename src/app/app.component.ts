45f4f9bc615762d963f8af3d58c30ee379afa15d
Hema Latha <hemalathacse581@gmail.com>
2024-06-07 07:18:54 -0700

    Santender frontend test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { PhotoListComponent } from './photo-list/photo-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [PhotoListComponent],
 })
 export class AppComponent {
-  title = 'santender-frontend-test';
+  title = 'Santender Frontend Test';
 }
